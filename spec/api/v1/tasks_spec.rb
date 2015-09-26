c5c99ba43acc9b04ec1bab6b405aee7acf576889
Andre Rodrigues <andre.rodrigues@tagview.com.br>
2015-09-26 15:38:02 -0300

    Pending tests for tasks api

---
+++ spec/api/v1/tasks_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Tasks API" do
+
+  it "lists all tasks"
+
+  it "shows a task"
+
+  it "creates a new task form a string input"
+
+  it "destroys a task"
+
+end
